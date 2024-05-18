3bf43bac38ae36016ef53cf8b048f9ec9e6b3773
thailq <xinphatmutikill@gmail.com>
2024-05-18 13:55:31 +0700

    Update

--- dist/constants/regax.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CHECK_WHITE_SPACE_REGEX = exports.CHECK_PHONE_NUMBER_REGEX = void 0;
-exports.CHECK_PHONE_NUMBER_REGEX = /^(-84|\+84|0)[3,5,7,8,9]\d{8,8}$/;
-exports.CHECK_WHITE_SPACE_REGEX = /^\S+$/;
